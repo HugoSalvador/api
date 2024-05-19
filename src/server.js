231bfcd93be4a6f1fd143d282e07612ad45efd4e
HugoSalvador <hugossalvador1241@gmail.com>
2024-05-19 14:05:13 -0300

    Primeiro commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/message/:id/:user", (request, response) => {
+
+    const { id, user } = request.params;
+
+    response.send(`
+    Id da mensagem: ${id}.
+    Para o usuario: ${user}.
+    `)
+})
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
