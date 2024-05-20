274835c559326054610117ddbb78b7626e697f36
HugoSalvador <hugossalvador1241@gmail.com>
2024-05-20 13:42:54 -0300

    Rota post

--- src/server.js
+++ src/server.js
@@ -2,15 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get("/message/:id/:user", (request, response) => {
 
-    const { id, user } = request.params;
 
-    response.send(`
-    Id da mensagem: ${id}.
-    Para o usuario: ${user}.
-    `)
-})
+app.post("/users", (request, response) => {
+    
+    response.send('Voce chamou POST');
+//
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
