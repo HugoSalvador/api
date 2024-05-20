de3fc4c80a305d47b3d4774532a584a4c6197637
HugoSalvador <hugossalvador1241@gmail.com>
2024-05-20 15:42:11 -0300

    Estruturas de controller, routes e utils

--- src/server.js
+++ src/server.js
@@ -1,14 +1,33 @@
+require("express-async-errors");
+
+const AppError = require("./utils/AppError");
+
 const express = require('express');
 
+//Não precisa informar qual arquivo ele vai ler, pois por padrão ele vai ler o index.js dentro routes
+const routes = require("./routes")
+
+
 const app = express();
+app.use(express.json());
+app.use(routes);
 
+app.use((error, request, response, next) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
 
+    console.error(error)
 
-app.post("/users", (request, response) => {
-    
-    response.send('Voce chamou POST');
-//
+    return response.status(500).json({
+        status: "Error",
+        message: "Internal server error"
+    })
 });
+
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
