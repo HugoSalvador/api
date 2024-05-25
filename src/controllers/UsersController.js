1353ffff430ea8ed2d2ce60c25835933c57d967e
HugoSalvador <hugossalvador1241@gmail.com>
2024-05-25 01:18:46 -0300

    Correcao da função para deleter em cascata

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -31,9 +31,11 @@ class UsersController {
         const { name, email, password, old_password } = request.body;
         const { id } = request.params;
 
+        
+
         const database = await sqliteConnection();
         const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
-        console.log(database);
+        
         
 
         if (!user) {
@@ -42,7 +44,6 @@ class UsersController {
 
         const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
         
-        
 
         if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
             throw new AppError("Este e-mail já está em uso.");
@@ -50,6 +51,11 @@ class UsersController {
     
         user.name = name ?? user.name;
         user.email = email ?? user.email;
+
+        
+        if (password && !old_password) {
+            throw new AppError("Você precisa informar a senha antiga para definir a nova senha");
+        }
 
         if (password && old_password) {
             const checkOldPassword = await compare(old_password, user.password);
@@ -66,8 +72,8 @@ class UsersController {
             name = ?,
             email = ?,
             password = ?,
-            updated_at = DATETIME('now'),
-            WHERE ID = ?`,
+            updated_at = DATETIME('now')
+            WHERE id = ?`,
             [user.name, user.email, user.password, id]
         )
 
